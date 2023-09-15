9f195b00f805ddac88e07047f2d80db552824024
rhmendonca <rafaelhenriquedemendonca@outlook.com>
2023-09-15 04:42:50 -0300

    Estruturando o projeto.

--- cp4-web/src/main.jsx
+++ cp4-web/src/main.jsx
@@ -3,8 +3,37 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+import Home from './routes/Home/Home.jsx'
+import VisualizarAparelho from './routes/EditarProdutos/EditarProdutos.jsx'
+import Erro404 from './routes/Error404/Error404.jsx'
+import Aparelhos from './routes/Aparelhos/Aparelhos.jsx'
+
+const router = createBrowserRouter([
+  {path: '/', 
+  element: <App/>,
+  errorElement: <Erro404/>,
+  children:[
+    {
+      path: '/', 
+      element: <Home/>
+    },
+    {
+    path: '/aparelhos', 
+    element: <Aparelhos/>},
+    {
+    path: '/aparelhos/visualizar/:id', 
+    element: <VisualizarAparelho/>},
+    {
+      path: "/antiga",
+      element: <Navigate to="/"/>
+    }
+    ]
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
