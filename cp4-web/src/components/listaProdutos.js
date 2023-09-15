6f9e91fbc05fc3020bf7713979b8e581b31a76d5
rhmendonca <rafaelhenriquedemendonca@outlook.com>
2023-09-15 05:34:26 -0300

    Concluindo o projeto.

--- cp4-web/src/components/listaProdutos.js
+++ cp4-web/src/components/listaProdutos.js
@@ -2,26 +2,31 @@ export const listaProdutos = [
     {
         id:1,
         produto: 'Smartphone Samsung',
-        preco: 899.99
+        preco: 899.99,
+        desc: "Celular 1"
     },
     {
         id: 2,
         produto: 'Smartphone Samsung Galaxy',
-        preco: 799.99
+        preco: 799.99,
+        desc: "Celular2 "
     },
     {
         id:3, 
         produto: 'Tablet Samsung',
         preco: 999.99,
+        desc: "Tablet 1"
     },
     {
         id:4, 
         produto: 'Tablet Samsung',
         preco: 699.99,
+        desc: "Tablet 2"
     },
     {
         id:5, 
         produto: 'Tablet Samsung',
         preco: 819.99,
+        desc: "Tablet 3"
     }
 ]
--- cp4-web/src/routes/Aparelhos/Aparelhos.jsx
+++ cp4-web/src/routes/Aparelhos/Aparelhos.jsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom"
 import { listaProdutos } from "../../components/listaProdutos.js"
 
-export default function Produtos(){
-
+export default function Aparelhos(){
     return(
         <main>
-            <h1>Produtos</h1>
+            <h1>Aparelhos</h1>
             {listaProdutos.map(prod => (
                 <div key={prod.id}>
-                    <Link to={`/produtos/editar/${prod.id}`}>
-                        Editar o produto: {prod.nome} 
+                    <Link to={`/aparelhos/visualizar/:id/${prod.id}`}>
+                        ID do Produto: {prod.produto} 
                     </Link>
+                    <Link to={`/aparelhos/visualizar/:id/${prod.id}`}>
+                        Produto: {prod.produto} 
+                    </Link>
+                    <Link to={`/aparelhos/visualizar/:id/${prod.id}`}>
+                        Preço: {prod.preco} 
+                    </Link>
+                    <Link to={`/aparelhos/visualizar/:id/${prod.id}`}>
+                        Descrição: {prod.desc} 
+                    </Link>
+
                 </div>
             ))}
         </main>
